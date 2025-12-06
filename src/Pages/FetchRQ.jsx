96116fe4eabfb6d499b4e8837111ffad7fad1474
Faizanul Haque <faizan07>
2025-12-06 21:54:40 +0530

    feat: implement FetchRQ with tanstack query

--- src/Pages/FetchRQ.jsx
+++ src/Pages/FetchRQ.jsx
@@ -1,3 +1,39 @@
+import { useQuery } from "@tanstack/react-query";
+import { fetchPosts } from "../API/api";
+
 export const FetchRQ = () => {
-    return <h1>FetchRQ</h1>
-}
+  const getPostsData = async () => {
+    try {
+      const res = await fetchPosts();
+      return res.status == 200 ? res.data : [];
+    } catch (error) {
+      console.log(error);
+      return [];
+    }
+  };
+
+  const {data} = useQuery({
+    queryKey: ["posts"],
+    queryFn: getPostsData,
+  });
+
+  return (
+    <>
+      <h1>FetchRQ</h1>
+      <div>
+        <ul className="section-accordion">
+          {data?.map((post) => {
+            const { userId, id, title, body } = post;
+            return (
+              <li key={id}>
+                <p>{userId}</p>
+                <p>{title}</p>
+                <p>{body}</p>
+              </li>
+            );
+          })}
+        </ul>
+      </div>
+    </>
+  );
+};
